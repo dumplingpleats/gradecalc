7dc408b8c0a456838de0702884622515924597f8
dumplingpleats <107207342+dumplingpleats@users.noreply.github.com>
2024-06-27 09:56:40 -0400

    Update Grades.js

--- Grades.js
+++ Grades.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const router = express.Router();
+// const mongoose = require('mongoose');
+// const express = require('express');
+// const router = express.Router();
 
-// Grade schema and model
-const gradeSchema = new mongoose.Schema({
-  course: String,
-  grade: String,
-  credits: Number
-});
-const Grade = mongoose.model('Grade', gradeSchema);
+// // Grade schema and model
+// const gradeSchema = new mongoose.Schema({
+//   course: String,
+//   grade: String,
+//   credits: Number
+// });
+// const Grade = mongoose.model('Grade', gradeSchema);
 
-// Add a new grade
-router.post('/', async (req, res) => {
-  const { course, grade, credits } = req.body;
-  const newGrade = new Grade({ course, grade, credits });
-  await newGrade.save();
-  res.redirect('/');
-});
+// // Add a new grade
+// router.post('/', async (req, res) => {
+//   const { course, grade, credits } = req.body;
+//   const newGrade = new Grade({ course, grade, credits });
+//   await newGrade.save();
+//   res.redirect('/');
+// });
 
-// Get all grades
-router.get('/', async (req, res) => {
-  const grades = await Grade.find({});
-  res.json(grades);
-});
+// // Get all grades
+// router.get('/', async (req, res) => {
+//   const grades = await Grade.find({});
+//   res.json(grades);
+// });
 
-module.exports = router;
+// module.exports = router;
 
 
