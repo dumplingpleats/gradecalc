0d7482172872207d4625ce5631147e02b92edf90
dumplingpleats <107207342+dumplingpleats@users.noreply.github.com>
2024-06-27 09:56:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
 
-const app = express();
+// const app = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/gpaCalculator', { useNewUrlParser: true, useUnifiedTopology: true });
+// // Connect to MongoDB
+// mongoose.connect('mongodb://localhost:27017/gpaCalculator', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
-app.set('view engine', 'ejs');
+// // Middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(__dirname));
+// app.set('view engine', 'ejs');
 
-// Routes
-const gradesRouter = require('./grades');
-app.use('/grades', gradesRouter);
+// // Routes
+// const gradesRouter = require('./grades');
+// app.use('/grades', gradesRouter);
 
-// Home route
-app.get('/', async (req, res) => {
-  const grades = await mongoose.model('Grade').find({});
-  res.render('index', { grades });
-});
+// // Home route
+// app.get('/', async (req, res) => {
+//   const grades = await mongoose.model('Grade').find({});
+//   res.render('index', { grades });
+// });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
